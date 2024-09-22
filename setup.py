c7d762b99fa338df4c6e469efbd10b3481f69a09
sachinbhirud1998 <149226903+sachinbhirud1998@users.noreply.github.com>
2024-09-22 19:55:55 +0530

    Update setup.py1

--- setup.py
+++ setup.py
@@ -3,14 +3,12 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-end-ML-Project-Implementation"
 AUTHOR_USER_NAME = "entbappy"
 SRC_REPO = "mlProject"
 AUTHOR_EMAIL = "entbappy73@gmail.com"
-
 
 setuptools.setup(
     name=SRC_REPO,
@@ -19,11 +17,26 @@ setuptools.setup(
     author_email=AUTHOR_EMAIL,
     description="A small python package for ml app",
     long_description=long_description,
-    long_description_content="text/markdown",
+    long_description_content_type="text/markdown",  # Corrected here
     url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src")
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        # Add your package dependencies here if not listed in requirements.txt
+        "pandas",
+        "notebook",
+        "numpy",
+        "scikit-learn",
+        "matplotlib",
+        "python-box==6.0.2",
+        "pyYAML",
+        "ensure==1.0.2",
+        "joblib",
+        "types-PyYAML",
+        "Flask",
+        "Flask-Cors"
+    ],
 )
